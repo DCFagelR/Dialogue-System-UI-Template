bfaed72986e0d385043deb9704b50419e7f7f66a
DCFagelR <50121963+DCFagelR@users.noreply.github.com>
2020-07-27 21:11:55 -0700

    Add files via upload

---
+++ Assets/Scripts/Core/DialogueSystem.cs
@@ -0,0 +1,126 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueSystem : MonoBehaviour
+{
+
+// ++Variables+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+    public const string TRANS_TAG = "<color=#00000000>";
+    public const string COLOR_TAG_END = "</color>";
+
+    // static instance so it can be located by instance rather than having a search procedure
+    public static DialogueSystem instance;
+    public string tempDialogue = "";
+    [HideInInspector] public bool isWaitingForUser = false;
+    Coroutine talking;
+
+    public ELEMENTS elements;
+    [System.Serializable]
+    public class ELEMENTS {
+        public GameObject dialoguePanel;
+        public Text nameText;
+        public Text dialogueText;
+    }
+
+// ++Functions+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+    void Awake() {
+        instance = this; // ensures only 1 in the scene
+    }
+
+// ----------------------------------------------------------------------------
+
+    // Start is called before the first frame update
+    void Start() {
+    }
+
+// ----------------------------------------------------------------------------
+    
+    // Update is called once per frame
+    void Update() {
+    }
+
+// ----------------------------------------------------------------------------
+
+    // Get elements; for easier access
+    public GameObject dialoguePanel {get{return elements.dialoguePanel;}}
+    public Text nameText {get{return elements.nameText;}}
+    public Text dialogueText {get{return elements.dialogueText;}}
+
+// ----------------------------------------------------------------------------
+
+    // Shows speech in the text box while someone is currently speaking
+    // input: dialogue -> text you want to display in the textbox
+    //       name -> name of the current speaker
+    public void Say(string dialogue, string name) {
+        StopTalking();
+        talking = StartCoroutine(TalkingRoutine(dialogue, name));
+    }
+
+// ----------------------------------------------------------------------------
+
+    public void StopTalking() {
+        if(isTalking) {
+            removeColorTags();
+            dialogueText.text = tempDialogue;
+            StopCoroutine(talking);
+        }
+        talking = null;
+    }
+
+// ----------------------------------------------------------------------------
+
+    public bool isTalking {
+        get{return talking != null;}
+    }
+
+// ----------------------------------------------------------------------------
+
+    public void removeColorTags() {
+        tempDialogue = tempDialogue.Replace(TRANS_TAG, "");
+        tempDialogue = tempDialogue.Replace(COLOR_TAG_END, "");
+    }
+
+// ----------------------------------------------------------------------------
+
+    // coroutine for Say(). While running = someone is currently speaking
+    IEnumerator TalkingRoutine(string dialogue, string name = "") {
+        int index = 1;
+
+        isWaitingForUser = false;
+
+        dialoguePanel.SetActive(true); // Move this eventually?
+        nameText.text = name;
+
+        // Initial text is all invisible on creation
+        tempDialogue = "<color=#00000000>" + dialogue + "</color>";
+        dialogueText.text = ""; // Empty last entered dialogue
+
+        while(index < dialogue.Length + 1) {
+            // Iterate the transparent tag through the text 1 char at a time
+            // This fixes the word wrap being janky when writing 1 char at a time
+            tempDialogue = tempDialogue.Remove(index-1, 17);
+            tempDialogue = tempDialogue.Insert(index, "<color=#00000000>");
+            dialogueText.text = tempDialogue;
+
+            index++;
+            yield return new WaitForEndOfFrame();
+        }
+
+        // remove color tags at the end
+        removeColorTags();
+        dialogueText.text = tempDialogue;
+
+        isWaitingForUser = true;
+        while(isWaitingForUser) {
+            yield return new WaitForEndOfFrame();
+        }
+
+        StopTalking();
+    }
+    
+// ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++    
+
+}
---
+++ Assets/Scripts/Testing/dialogue.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class dialogue : MonoBehaviour
+{
+    DialogueSystem dialoguetest;
+
+    public class characters {
+        public string name;
+        public string[] testDialogue;
+    }
+
+    characters Tomoya = new characters();
+    characters Kyou = new characters();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialoguetest = DialogueSystem.instance;   
+
+        Tomoya.name = "Tomoya";
+        Tomoya.testDialogue = new string[] {
+            "I hate this town. It’s too filled with memories I’d rather forget. " + 
+            "I go to school every day, hang out with my friends, and then go home. " +
+            "There’s no place I’d rather not go ever again. I wonder if anything will ever change? Will that day ever come?",
+            "If I’m around you, I don’t think I’ll ever be bored."
+        };
+
+        Kyou.name = "Kyou";
+        Kyou.testDialogue = new string[] {
+            "Isn’t there something strange in becoming friends because you’re asked? Friends aren’t given; you’re supposed to make them",
+            "If the results come true, it’s as if there’s only one future. If it fails, we can think that other futures exist… I want to believe that in our future there are many possibilities waiting."
+        };
+
+    }
+
+    public int index = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)) {
+            if(!dialoguetest.isTalking || dialoguetest.isWaitingForUser) {
+                if( index >= 2) {
+                    return;
+                }
+            }
+            dialoguetest.Say(Tomoya.testDialogue[index], Tomoya.name);
+            index++;
+        }
+    }
+}
